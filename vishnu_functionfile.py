c687736842910ba2b5e04e40ab96010b543472f2
vishnukothuri0404 <53860893+vishnukothuri0404@users.noreply.github.com>
2024-09-15 00:13:28 -0400

    Update vishnu_functionfile.py

--- vishnu_functionfile.py
+++ vishnu_functionfile.py
@@ -4,5 +4,5 @@ def vishnu_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Edit from Lambda!')
     }
